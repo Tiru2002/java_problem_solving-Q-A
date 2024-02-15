c2e329da85dbdfed7d7c8f43389c70f8c7c7f9c2
Tiru2002 <tirumalarao1805@gmail.com>
2024-02-15 22:35:01 +0530

    first commit

---
+++ src/algorithms/Q1.java
@@ -0,0 +1,18 @@
+//Q1.Ask user to enter name and greet him with "Hello"
+
+package algorithms;
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String name = sc.nextLine();
+        System.out.println("\nHello, "+name);
+        sc.close();
+
+    } 
+
+    
+}
