e342fbc864f33339c2b19b1fa9e370c621157e89
Tiru2002 <tirumalarao1805@gmail.com>
2024-02-17 22:56:06 +0530

    commiting Q5

---
+++ src/algorithms/Q5.java
@@ -0,0 +1,18 @@
+package algorithms;
+
+// Q5.Calculate Total marks of a student with 5 subjects
+
+public class Q5 {
+
+int subject1=80,subject2=90,subject3=76,subject4=92,subject5=88;
+	
+    public static void main(String[] args) {
+        Q5 q5 = new Q5();
+        System.out.println("Total Marks : "+q5.totalMarks()+"/500");
+    }
+
+    private int totalMarks() {
+        return subject1+subject2+subject3+subject4+subject5;
+    
+}
+}
