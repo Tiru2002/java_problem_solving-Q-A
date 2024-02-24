3780f234c27d948efdef0469c0980cd1d0210e20
Tiru2002 <tirumalarao1805@gmail.com>
2024-02-24 13:41:41 +0530

    changes

--- src/Basics/Q10.java
+++ src/Basics/Q10.java
@@ -4,6 +4,7 @@ package Basics;
 import java.util.Scanner;
 public class Q10 {
     public static void main(String[] args) {
+<<<<<<< HEAD
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number:");
         int num=sc.nextInt();
@@ -20,9 +21,26 @@ public class Q10 {
                    System.out.println(num + "is a Prime number");
                    break;
                 }
+=======
+
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter the number");
+          int n=sc.nextInt();
+          int f=0;
+          if(n<=1){
+            System.out.println("The number is not prime");
+          }
+          for(int i=2;i<n;i++){
+            if(n%i==0){
+                f++;
+>>>>>>> 6d5a0c1 (changes)
             }
-      
-        }
-        sc.close();
-    }
-}
+          }
+          if(f==0){
+            System.out.println(n+"  is a prime number");
+          }
+          else{
+            System.err.println(n + "is not a prime number");
+          }
+       sc.close();
+    }}
---
+++ src/intermittent/Q1.java
@@ -0,0 +1,18 @@
+package intermittent;
+//write a program to reverse a number
+public class Q1 {
+
+    public static void main(String[] args) {
+        int num = 1234567890;
+        System.out.println("Original Number : " + num);
+        int rev=0;
+        int rem;
+        while (num!=0) {
+            rem=num%10;
+            rev=(rev*10)+rem;
+            num/=10;
+        }
+        System.out.println("\nReversed Number : " + rev);
+    }
+    
+}
